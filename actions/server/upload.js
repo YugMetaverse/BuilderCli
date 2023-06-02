e3eef8612393beb2a36ffc3f3c50e3c0c5f1b82a
ravi18 <ravi@arrowai.com>
2023-06-02 15:52:41 +0530

    changes

--- actions/server/upload.js
+++ actions/server/upload.js
@@ -1,12 +1,81 @@
-const uploadPlugin = (options) => {
+const fs = require('fs');
+const https = require('https');
+const FormData = require('form-data');
 
-}
+const uploadPlugin = async (options) => {
+    const buildPackPath = `${options.stagingdirectory}/${options.platform}/${options.pluginname}.pak`;
+    const buildPack = fs.readFileSync(buildPackPath);
+    const formData = new FormData();
 
-const uploadApp = (options) => {
+    formData.append('file', buildPack, { filename: 'plugin.pak' });
+    const requestOptions = {
+        hostname: 'https://webapi.yugverse.com',
+        path: '/upload',
+        method: 'POST',
+        headers: formData.getHeaders(),
+        ...options,
+    };
 
-}
+    const response = await new Promise((resolve, reject) => {
+        const request = https.request(requestOptions, (response) => {
+            let data = '';
+            response.on('data', (chunk) => {
+                data += chunk;
+            });
+            response.on('end', () => {
+                console.log('Request complete.');
+                resolve(data);
+            });
+        });
+
+        request.on('error', (error) => {
+            console.error(`Error with request: ${error}`);
+            reject(error);
+        });
+
+        formData.pipe(request);
+    });
+
+    return response;
+};
+
+
+const uploadApp = async (options) => {
+    const buildPackPath = `${options.archivedirectory}`;
+    const buildPack = fs.readFileSync(buildPackPath);
+    const formData = new FormData();
+
+    formData.append('file', buildPack, { filename: 'build.pak' });
+    const requestOptions = {
+        hostname: 'https://webapi.yugverse.com',
+        path: '/upload',
+        method: 'POST',
+        headers: formData.getHeaders(),
+        ...options,
+    };
+
+    const response = await new Promise((resolve, reject) => {
+        const request = https.request(requestOptions, (response) => {
+            let data = '';
+            response.on('data', (chunk) => {
+                data += chunk;
+            });
+            response.on('end', () => {
+                console.log('Request complete.');
+                resolve(data);
+            });
+        });
+        request.on('error', (error) => {
+            console.error(`Error with request: ${error}`);
+            reject(error);
+        });
+        formData.pipe(request);
+    });
+
+    return response;
+};
 
 module.exports = {
     uploadPlugin,
-    uploadApp
-}
+    uploadApp,
+};
